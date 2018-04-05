c2078fe12aa1faa76f7c0e0951a57ba7be64201a
timhayes00 <timhayes00@gmail.com>
2018-04-05 01:15:15 -0400

    still working on the modal

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # friend-finder
+This is a basic friend finding app, based on the "birds of a feather flock together" theory, ie the idea that you are more likely to get along with someone who closely matches your own outlook on life and the reasoning behind the choices you make.
+
+Users fill out a basic survey, with answers ranging between 1 and 5 for various statements.  A '1' answer means they strongly disagree with the statement, a '5' is given to indicate strong agreement with the statement.
+
+At the end of the quiz, the program goes through the stored answers of and shows the name and picture of the previous quiz taker who most closely matched the answers given.
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,36 +3,34 @@ var valuesComparison;
 
 
 
-module.exports = function(app) {
-  app.get("/api/friends", function(req, res) {
-    res.json(friendsData);
-  });
+module.exports = function (app) {
+    app.get("/api/friends", function (req, res) {
+        res.json(friendsData);
+    });
 
-  app.post("/api/friends", function(req, res) {
-      
-    console.log(req.body);
-    friendsData.push(req.body);
-    var searchArray = friendsData.length -1;
-      for (i = 0; i < searchArray; i++){
-          var bestMatch;
-          var closestMatch = 50;
-          var currentComp = 0;
-          console.log("iterating through the 'i' loop")
-          for (j = 0; j < 10; j++){
-              //console.log("friendsData[i].scores[j] " + friendsData[i].scores[j])
-              //console.log("friendsData[lastData].scores[j] " + friendsData[recentFriend].scores[j])
-              currentComp += Math.abs(parseInt(friendsData[i].scores[j]) - parseInt(req.body.scores[j]));
-          }
-          console.log("current value of 'currentComp' = " + currentComp)
-          if (currentComp < closestMatch)
-          {
-              bestMatch = friendsData[i];
-          }
-          
-      }
-    console.log(bestMatch);
+    app.post("/api/friends", function (req, res) {
+        var bestMatch;
+        var closestMatch = 50;
+        for (i = 0; i < friendsData.length; i++) {
+            var currentComp = 0;
+            for (j = 0; j < 10; j++) {
+                //console.log("friendsData[i].scores[j] " + friendsData[i].scores[j])
+                //console.log("req.body.scores[j] " + req.body.scores[j])
+                currentComp += Math.abs(parseInt(friendsData[i].scores[j]) - parseInt(req.body.scores[j]));
+            }
+            //updating the "closest match" if needed
+            if (currentComp < closestMatch) {
+                closestMatch = currentComp;
+                bestMatch = friendsData[i];
+            }
 
-  });
+        }
+        friendsData.push(req.body);
+        //Showing the user their closest match
+       // console.log(bestMatch);
+        
+
+    });
 
 
 };
